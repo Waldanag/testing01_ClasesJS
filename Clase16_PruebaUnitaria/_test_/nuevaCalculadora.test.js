0d568a6b766574fe3beb465b6303fb5be01e73e7
Waldanag <walter@algabits.com>
2023-06-12 19:03:09 -0400

    Comenzando test unitario

---
+++ Clase16_PruebaUnitaria/_test_/nuevaCalculadora.test.js
@@ -0,0 +1,25 @@
+const { suma } = require('../nuevaCalculadora.js');
+
+//it o test
+
+//test('', ()=>{});
+
+test('Suma - Valores Ok', ()=>{
+
+    let valorActual = suma(5, 55);
+    let valorEsperado = 60;
+
+    expect(valorActual).toBe(valorEsperado);
+    //expect(suma(5, 55)).toBe(60);
+
+});
+
+test('Suma - Valores Null', ()=>{
+
+    let valorActual = suma(null, null);
+    let valorEsperado = null;
+
+    expect(valorActual).toBe(valorEsperado);
+    //expect(suma(5, 55)).toBe(60);
+
+});
